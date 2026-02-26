aea35658d0d031f694e6689b90c27f8228ceb3e0
Carlos Garcia <dancingevilgenius@gmail.com>
2026-02-26 08:54:12 -0600

    Added code to insert rows into superheroes table from the previously parsed strings.

--- superherogenerator/src/main/java/com/ceg/ParseAndInsert.java
+++ superherogenerator/src/main/java/com/ceg/ParseAndInsert.java
@@ -6,6 +6,11 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.io.Reader;
 import java.lang.reflect.Type;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 //import java.sql.Connection;
@@ -14,6 +19,12 @@ import java.util.List;
 //import java.sql.SQLException;
 
 public class ParseAndInsert {
+
+    private static final String DB_URL = "jdbc:postgresql://localhost:5432/postgres";
+    private static final String USER = "postgres";
+    private static final String PASSWORD = "asdf1234";
+    public static ArrayList<String> heroJSON = new ArrayList<String>();
+
     public static void main(String[] args) {
         // Create a Gson instance
         Gson gson = new Gson();
@@ -35,12 +46,35 @@ public class ParseAndInsert {
                  * + " address: " + hero.getAddresses().getFirst().getCity());
                  */
                 String jsonString = gson.toJson(hero);
+                heroJSON.add(jsonString);
                 System.out.println(jsonString);
+                insertHero(jsonString);
                 // Or simply: System.out.println(student);
             }
 
         } catch (IOException e) {
             e.printStackTrace();
+        }
+    }
+
+    public static void insertHero(String jsonStr) {
+        // SQL INSERT statement with placeholders (?)
+        String SQL = "INSERT INTO superheroes (hero_data) VALUES (?::jsonb)";
+
+        // Use try-with-resources to ensure Connection and PreparedStatement are closed
+        // automatically
+        try (Connection conn = DriverManager.getConnection(DB_URL, USER, PASSWORD);
+                PreparedStatement pstmt = conn.prepareStatement(SQL)) {
+
+            // Bind values to the placeholders
+            pstmt.setString(1, jsonStr); // Binds the first '?' to the name value
+
+            // Execute the update
+            int affectedRows = pstmt.executeUpdate();
+            System.out.println(affectedRows + " row(s) inserted.");
+
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
         }
     }
 
